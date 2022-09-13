362c74c668fab47d2ad7506bdf65c282cb1bfbd7
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2022-09-14 00:15:51 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,7 @@
+let buttons = document.getElementById("button")
+let inputEl = document.getElementById("input-btn")
+let clearBtn = document.getElementById("clear") 
+buttons.addEventListener("click", function() {
+   inputEl.textContent = 9
+})
+
